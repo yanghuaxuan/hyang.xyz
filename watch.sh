e79dd4c6be6926e3773c024f1c08049897be3d48
hyang <hyang@hyang.xyz>
2023-09-14 19:54:29 -0700

    unlucky

--- watch.sh
+++ watch.sh
@@ -4,6 +4,7 @@ while true; do
   (git fetch && git reset --hard origin) || git clone "$REPO" . && git submodule update --init
   git submodule update --remote
   hugo
+  rm -Rf /var/www/hugo_pub
   mv -f ./public /var/www/hugo_pub
   chown -R root:www-data /var/www/hugo_pub
   sleep 60
