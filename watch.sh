b969e3d5a9749e1acc81aa5eab47b10a137f2c0f
hyang <hyang@hyang.xyz>
2023-09-14 19:35:57 -0700

    forgot to add this

--- watch.sh
+++ watch.sh
@@ -3,6 +3,9 @@
 while true; do
   (git fetch && git reset --hard origin) || git clone "$REPO" . && git submodule update --init
   git submodule update --remote
+  hugo
+  mv -f ./public /var/www/hugo_pub
+  chown -R root:www-data /var/www/hugo_pub
   sleep 60
 done
 
