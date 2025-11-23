481c8be7c31ec24db000261d4f914db6940d5d1f
yanghuaxuan <yanghuaxuan@gmail.com>
2025-11-22 19:23:32 -0800

    liking it

--- build-site.el
+++ build-site.el
@@ -8,6 +8,17 @@
 (setq org-html-head-include-default-style nil
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
+(setq org-html-preamble
+      (concat "<header>"
+                  "<nav style=\"display: flex\">"
+		  "<a href=\"./index.html\">index</a>"
+		  "<a href=\"./blog.html\">blogpostings</a>"
+		  "<a href=\"./resume.pdf\">resume.pdf</a>"
+		  "<a href=\"./sitemap.html\" style=\"margin-left: auto\">...</a>"
+		  "</nav>"
+	      "</header>"))
+
+
 (setq org-publish-project-alist
       (list
        (list "hyang.xyz"
@@ -16,6 +27,7 @@
 	     :publishing-directory "./public"
 	     :publishing-function 'org-html-publish-to-html
 	     :auto-sitemap t
+	     ;; :html-preamble t
 	     :with-author nil
 	     :time-stamp-file nil
 	     :html-validation-link nil
