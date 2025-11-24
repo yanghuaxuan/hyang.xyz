cb243eebfce47f54cee82613b809ac8d381dac94
yanghuaxuan <yanghuaxuan@gmail.com>
2025-11-23 19:18:33 -0800

    cinema

--- build-site.el
+++ build-site.el
@@ -6,16 +6,19 @@
 (require 'htmlize)
 
 (setq org-html-head-include-default-style nil
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head (concat
+		     "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+		     "<link rel=\"stylesheet\" href=\"./custom.css\">"))
 
 (setq org-html-preamble
-      (concat "<header>"
+      (concat "<header style=\"padding-top: 5px\">"
+	          "<a href=\"./index.html\"><b>/home/hyang</b></a>"
                   "<nav style=\"display: flex\">"
-		  "<a href=\"./index.html\">index</a>"
-		  "<a href=\"./blog.html\">blogpostings</a>"
-		  "<a href=\"./resume.pdf\">resume.pdf</a>"
-		  "<a href=\"./sitemap.html\" style=\"margin-left: auto\">...</a>"
-		  "</nav>"
+		      "<a href=\"./blog.html\">blogpostings</a>"
+		      "<a href=\"./blog.html\">projects</a>"
+		      "<a href=\"./resume.pdf\">resume.pdf</a>"
+		      "<a href=\"./sitemap.html\" style=\"margin-left: auto\">...</a>"
+		      "</nav>"
 	      "</header>"))
 
 
@@ -35,7 +38,7 @@
        (list "static"
 	     :base-directory "./static"
 	     :recursive t
-	     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg"
 	     :publishing-directory "./public"
 	     :publishing-function 'org-publish-attachment
 	     )))
